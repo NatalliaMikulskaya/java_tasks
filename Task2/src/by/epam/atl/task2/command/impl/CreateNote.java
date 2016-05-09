710b8ce01480ad2ddc1df3419a41c5caa0a1b9fd
Natalia <Natalia@Natalia_Station>
2016-05-09 15:29:21 +0300

    createNote changed

--- Task2/src/by/epam/atl/task2/command/impl/CreateNote.java
+++ Task2/src/by/epam/atl/task2/command/impl/CreateNote.java
@@ -12,6 +12,7 @@ import by.epam.atl.task2.bean.Response;
 import by.epam.atl.task2.command.Command;
 import by.epam.atl.task2.service.NoteBookService;
 import by.epam.atl.task2.service.ServiceFactory;
+import by.epam.atl.task2.service.exception.ServiceException;
 
 public class CreateNote implements Command {
 	private static final Logger LOG = LogManager.getLogger(MethodHandles.lookup().lookupClass());
@@ -25,17 +26,21 @@ public class CreateNote implements Command {
 		Date date = request.getDate();
 		String content = request.getContent();
 		
-		Note nt = noteBookService.createNote(date, content);
-				
 		Response rsp = new Response();
 		
-		if (nt != null ){
-			rsp.setErrorMessage(null);
-			rsp.setMessage("Note was created.");
-			rsp.setNote(nt);
-		} else{
+		try{
+			Note nt = noteBookService.createNote(date, content);
+		
+			if (nt != null ){
+				rsp.setErrorMessage(null);
+				rsp.setMessage("Note was created.");
+				rsp.setNote(nt);
+			}
+		}
+		catch (ServiceException e){
 			rsp.setErrorMessage("Error occured while note was created.");
 			rsp.setMessage(null);
+			LOG.error("Exception:"+e+" "+e.getCause());
 		}
 		
 		return rsp;
