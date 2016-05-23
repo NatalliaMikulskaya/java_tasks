77e06b78281e4cd9789c3c6a5f3e33ab7bf309e0
Natallia Mikulskaya <Natallia_Mikulskaya.epam.com>
2016-05-23 08:36:01 +0300

    23/05

--- Task2/src/by/epam/atl/task2/service/ServiceFactory.java
+++ Task2/src/by/epam/atl/task2/service/ServiceFactory.java
@@ -1,10 +1,5 @@
 package by.epam.atl.task2.service;
 
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import by.epam.atl.task2.bean.Note;
-import by.epam.atl.task2.bean.NoteBook;
 import by.epam.atl.task2.service.impl.NoteBookServiceImpl;
 
 public class ServiceFactory {
